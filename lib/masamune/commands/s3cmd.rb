8807719679afb64a53836dc501935b6e3e7e9c71
Michael Andrews <michael@socialcast.com>
2013-06-28 10:25:44 -0700

    Retry s3cmd

--- lib/masamune/commands/s3cmd.rb
+++ lib/masamune/commands/s3cmd.rb
@@ -1,5 +1,39 @@
 module Masamune::Commands
   class S3Cmd
+    MAX_RETRIES = 3
+    DEFAULT_BACKOFF = 5
+
+    attr_accessor :extra, :block, :backoff
+
+    def initialize(opts = {})
+      self.extra    = opts[:extra]
+      self.block    = opts[:block]
+      self.backoff  = opts.fetch(:backoff, DEFAULT_BACKOFF)
+      @retry_count = 0
+    end
+
+    def command_args
+      args = []
+      args << Masamune.configuration.s3cmd[:path]
+      args << Masamune.configuration.s3cmd[:options].map(&:to_a)
+      args << extra
+      args.flatten
+    end
+
+    def handle_stdout(line, line_no)
+      block.call(line) if block
+    end
+
+    def around_execute(&block)
+      begin
+        yield
+      rescue
+        sleep backoff
+        @retry_count += 1
+        retry unless @retry_count > MAX_RETRIES
+      end
+    end
+
     module ClassMethods
       def s3n(file, options = {})
         file.dup.tap do |out|
@@ -14,25 +48,6 @@ module Masamune::Commands
           out.sub!(%r{/?\z}, '/') if options[:dir]
         end
       end
-    end
-
-    attr_accessor :extra, :block
-
-    def initialize(opts = {})
-      self.extra = opts[:extra]
-      self.block = opts[:block]
-    end
-
-    def command_args
-      args = []
-      args << Masamune.configuration.s3cmd[:path]
-      args << Masamune.configuration.s3cmd[:options].map(&:to_a)
-      args << extra
-      args.flatten
-    end
-
-    def handle_stdout(line, line_no)
-      block.call(line) if block
     end
   end
 end
