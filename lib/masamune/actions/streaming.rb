15e1bfd6c9618a94db4757633cce36b53196ea2c
Michael Andrews <michael@socialcast.com>
2013-07-30 14:45:07 -0700

    Use resolved jobflow instead of jobflow input

--- lib/masamune/actions/streaming.rb
+++ lib/masamune/actions/streaming.rb
@@ -3,15 +3,15 @@ module Masamune::Actions
     def streaming(opts = {})
       opts = opts.to_hash.symbolize_keys
 
-      jobflow = opts[:jobflow] || Masamune.configuration.jobflow
+      opts.merge!(jobflow: Masamune.configuration.jobflow)
 
-      command = if jobflow
+      command = if opts[:jobflow]
         Masamune::Commands::Streaming.new(opts.merge(quote: true, file_args: false))
       else
         Masamune::Commands::Streaming.new(opts)
       end
 
-      command = Masamune::Commands::ElasticMapReduce.new(command, jobflow: jobflow) if jobflow
+      command = Masamune::Commands::ElasticMapReduce.new(command, opts) if opts[:jobflow]
       command = Masamune::Commands::RetryWithBackoff.new(command, opts)
       command = Masamune::Commands::Shell.new(command, opts)
       command.execute
