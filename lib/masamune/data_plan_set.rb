e1894bf1ecb9c44e2dfde1cbfa8b662ec7ad6a88
Michael Andrews <michael@socialcast.com>
2013-08-09 15:20:35 -0700

    Round out DataPlanSet construction

--- lib/masamune/data_plan_set.rb
+++ lib/masamune/data_plan_set.rb
@@ -1,40 +1,66 @@
-require 'delegate'
-
 class Masamune::DataPlanSet < Set
-  require 'masamune/accumulate'
-  include Masamune::Accumulate
-
   EMPTY = self.new
 
-  def initialize(enum = nil)
-    case enum
-    when Array
-      super enum.flatten.uniq
-    when Set, self.class
-      super
-      merge(enum)
-    else
-      super
-    end
+  def initialize(rule, enum = nil)
+    @rule = rule
+    super convert_enum(enum)
   end
 
   def union(enum = nil)
     super(enum || EMPTY)
   end
 
-  def missing(&block)
-    self.each do |elem|
-      yield elem if Masamune.filesystem.glob(elem.path).empty?
-    end
+  def add(elem = nil)
+    super convert_elem(elem)
   end
-  method_accumulate :missing
 
-  def existing(&block)
-    self.each do |elem|
-      Masamune.filesystem.glob(elem.path) do |path|
-        yield elem.rule.bind_path(path)
+  def include?(elem = nil)
+    super convert_elem(elem)
+  end
+
+  def missing
+    self.class.new(@rule).tap do |set|
+      self.each do |elem|
+        set.add elem if Masamune.filesystem.glob(elem.path).empty?
       end
     end
   end
-  method_accumulate :existing
+
+  def existing
+    self.class.new(@rule).tap do |set|
+      self.each do |elem|
+        Masamune.filesystem.glob(elem.path) do |path|
+          set.add elem.rule.bind_path(path)
+        end
+      end
+    end
+  end
+
+  private
+
+  def convert_elem(elem)
+    case elem
+    when nil
+    when Masamune::DataPlanElem
+      elem
+    when String
+      @rule.bind_path(elem)
+    else
+      raise "Unhandled elem class #{elem.class}"
+    end
+  end
+
+  def convert_enum(enum)
+    case enum
+    when nil
+    when Array
+      enum.flatten.uniq
+    when Set, self.class
+      enum
+    when String
+      [enum]
+    else
+      raise "Unhandled enum class #{enum.class}"
+    end
+  end
 end
--- spec/masamune/data_plan_set_spec.rb
+++ spec/masamune/data_plan_set_spec.rb
@@ -9,38 +9,101 @@ describe Masamune::DataPlanSet do
   end
 
   let(:plan) { Masamune::DataPlan.new }
-  let(:instance) { Masamune::DataPlanSet.new(set) }
-
-  before do
-    plan.add_target_rule('primary', 'table/y=%Y/m=%m/d=%d')
-    plan.add_source_rule('primary', 'log/%Y%m%d.*.log')
-  end
+  let(:source_rule) { Masamune::DataPlanRule.new(plan, 'primary', :source, 'log/%Y%m%d.*.log') }
+  let(:target_rule) { Masamune::DataPlanRule.new(plan, 'primary', :target, 'table/y=%Y/m=%m/d=%d') }
 
   describe '#missing' do
     let(:paths) { ['table/y=2013/m=01/d=01', 'table/y=2013/m=01/d=02', 'table/y=2013/m=01/d=03'] }
 
-    let(:set) { plan.targets_from_paths('primary', paths) }
+    let(:instance) { Masamune::DataPlanSet.new(target_rule, paths) }
 
     subject(:missing) do
       instance.missing
     end
 
     context 'when all missing' do
-      it { missing.map(&:path).should == paths }
+      it { missing.should have(3).items }
+      it { missing.should include 'table/y=2013/m=01/d=01' }
+      it { missing.should include 'table/y=2013/m=01/d=02' }
+      it { missing.should include 'table/y=2013/m=01/d=03' }
     end
 
     context 'when some missing' do
       before do
         fs.touch!('table/y=2013/m=01/d=01', 'table/y=2013/m=01/d=02')
       end
-      it { missing.map(&:path).should == ['table/y=2013/m=01/d=03'] }
+      it { missing.should have(1).items }
+      it { missing.should include 'table/y=2013/m=01/d=03' }
     end
 
     context 'when none missing' do
       before do
         fs.touch!('table/y=2013/m=01/d=01', 'table/y=2013/m=01/d=02', 'table/y=2013/m=01/d=03')
       end
-      it { should be_empty }
+      it { missing.should be_empty }
+    end
+  end
+
+  describe '#existing' do
+    let(:instance) { Masamune::DataPlanSet.new(source_rule, paths) }
+
+    subject(:existing) do
+      instance.existing
+    end
+
+    context 'with basic paths' do
+      let(:paths) { ['log/20130101.random_1.log', 'log/20130102.random_1.log'] }
+
+      context 'when none existing' do
+        it { existing.should be_empty }
+      end
+
+      context 'when some existing' do
+        before do
+          fs.touch!('log/20130101.random_1.log', 'log/20130101.random_2.log')
+        end
+        it { existing.should have(1).items }
+        it { existing.should include 'log/20130101.random_1.log' }
+      end
+
+      context 'when all existing' do
+        before do
+          fs.touch!('log/20130101.random_1.log', 'log/20130101.random_2.log')
+          fs.touch!('log/20130102.random_1.log', 'log/20130102.random_2.log')
+        end
+        it { existing.should have(2).items }
+        it { existing.should include 'log/20130101.random_1.log' }
+        it { existing.should include 'log/20130102.random_1.log' }
+      end
+    end
+
+    context 'with wildcard paths' do
+      let(:paths) { ['log/20130101.*.log', 'log/20130102.*.log'] }
+
+      context 'when none existing' do
+        it { existing.should be_empty }
+      end
+
+      context 'when some existing' do
+        before do
+          fs.touch!('log/20130101.random_1.log', 'log/20130101.random_2.log')
+        end
+        it { existing.should have(2).items }
+        it { existing.should include 'log/20130101.random_1.log' }
+        it { existing.should include 'log/20130101.random_2.log' }
+      end
+
+      context 'when all existing' do
+        before do
+          fs.touch!('log/20130101.random_1.log', 'log/20130101.random_2.log')
+          fs.touch!('log/20130102.random_1.log', 'log/20130102.random_2.log')
+        end
+        it { existing.should have(4).items }
+        it { existing.should include 'log/20130101.random_1.log' }
+        it { existing.should include 'log/20130101.random_2.log' }
+        it { existing.should include 'log/20130102.random_1.log' }
+        it { existing.should include 'log/20130102.random_2.log' }
+      end
     end
   end
 end
