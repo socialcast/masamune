41aa0cd6df30dcd73c67e932ad4df5c68a2c9b56
Michael Andrews <michael@socialcast.com>
2013-08-11 14:26:40 -0700

    Implement some micro cache optimizations

--- lib/masamune/data_plan.rb
+++ lib/masamune/data_plan.rb
@@ -10,6 +10,7 @@ class Masamune::DataPlan
     @command_rules = Hash.new
     @targets = Hash.new { |set,rule| set[rule] = Masamune::DataPlanSet.new(@target_rules[rule]) }
     @sources = Hash.new { |set,rule| set[rule] = Masamune::DataPlanSet.new(@source_rules[rule]) }
+    @set_cache = Hash.new { |cache,level| cache[level] = Hash.new }
   end
 
   def add_target_rule(rule, target, target_options = {})
@@ -24,6 +25,7 @@ class Masamune::DataPlan
     @command_rules[rule] = [command, command_options]
   end
 
+  # TODO use constructed reference instead
   def rule_for_target(target)
     matches = @target_rules.select { |rule, matcher| matcher.matches?(target) }
     Masamune.logger.debug("No rule matches target #{target}") and return Masamune::DataPlanRule::TERMINAL if matches.empty?
@@ -31,7 +33,7 @@ class Masamune::DataPlan
     matches.map(&:first).first
   end
 
-  # TODO covert to DataPlanSet
+  # TODO convert to DataPlanSet
   def targets_for_date_range(rule, start, stop, &block)
     target_template = @target_rules[rule]
     target_template.generate(start.to_time.utc, stop.to_time.utc) do |target_instance|
@@ -40,27 +42,45 @@ class Masamune::DataPlan
   end
   method_accumulate :targets_for_date_range
 
-  # TODO micro cache - clear on prepare phase
   def targets_for_source(rule, source)
     source_template = @source_rules[rule]
     target_template = @target_rules[rule]
     source_instance = source.is_a?(Masamune::DataPlanElem) ? source : source_template.bind_path(source)
-    Masamune::DataPlanSet.new(target_template).tap do |set|
-      source_template.generate_via_unify_path(source_instance.path, target_template) do |target|
-        set.add target
+
+    @set_cache[:targets_for_source][rule + ':' + source_instance.path] ||= begin
+      Masamune::DataPlanSet.new(target_template).tap do |set|
+        source_template.generate_via_unify_path(source_instance.path, target_template) do |target|
+          set.add target
+        end
       end
     end
   end
 
-  # TODO micro cache - clear on prepare phase
   def sources_for_target(rule, target)
     source_template = @source_rules[rule]
     target_template = @target_rules[rule]
     target_instance = target.is_a?(Masamune::DataPlanElem) ? target : target_template.bind_path(target)
-    Masamune::DataPlanSet.new(source_template).tap do |set|
-      target_template.generate_via_unify_path(target_instance.path, source_template) do |source|
-        set.add source
+
+    @set_cache[:sources_for_target][rule + ':' + target_instance.path] ||= begin
+      Masamune::DataPlanSet.new(source_template).tap do |set|
+        target_template.generate_via_unify_path(target_instance.path, source_template) do |source|
+          set.add source
+        end
       end
+    end
+  end
+
+  def targets(rule)
+    @set_cache[:targets_for_rule][rule] ||= begin
+      result = @sources[rule].map { |source| targets_for_source(rule, source) }.reduce(&:union)
+      @targets[rule].union(result)
+   end
+  end
+
+  def sources(rule)
+    @set_cache[:sources_for_rule][rule] ||= begin
+      result = @targets[rule].map { |target| sources_for_target(rule, target) }.reduce(&:union)
+      @sources[rule].union(result).adjacent
     end
   end
 
@@ -71,27 +91,15 @@ class Masamune::DataPlan
 
   def execute(rule, options = {})
     return if targets(rule).missing.empty?
-    # TODO replace with source.rule.name
     sources(rule).missing.group_by { |source| rule_for_target(source.path) }.each do |derived_rule, sources|
       if derived_rule != Masamune::DataPlanRule::TERMINAL
-        prepare(derived_rule, {targets: sources.map(&:path)})
+        prepare(derived_rule, targets: sources.map(&:path))
         execute(derived_rule, options)
       end
     end
 
     command, command_options = @command_rules[rule]
     command.call(self, rule, options)
-  end
-
-  # TODO micro cache - clear on prepare phase
-  def targets(rule)
-    result = @sources[rule].map { |source| targets_for_source(rule, source) }.reduce(&:union)
-    @targets[rule].union(result)
-  end
-
-  # TODO micro cache - clear on prepare phase
-  def sources(rule)
-    result = @targets[rule].map { |target| sources_for_target(rule, target) }.reduce(&:union)
-    @sources[rule].union(result).adjacent
+    @set_cache.clear
   end
 end
