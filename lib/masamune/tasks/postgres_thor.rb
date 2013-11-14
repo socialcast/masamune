caf8976d33b829b2100df9314fd35c86edee3807
Michael Andrews <michael@socialcast.com>
2013-11-14 15:07:45 -0800

    Add postgres thor task

---
+++ lib/masamune/tasks/postgres_thor.rb
@@ -0,0 +1,36 @@
+require 'masamune'
+require 'thor'
+
+module Masamune::Tasks
+  class PostgresThor < Thor
+    include Masamune::Thor
+    include Masamune::Actions::Postgres
+
+    # FIXME need to add an unnecessary namespace until this issue is fixed:
+    # https://github.com/wycats/thor/pull/247
+    namespace :postgres
+
+    desc 'psql', 'Launch a Postgres session'
+    method_option :file, :aliases => '-f', :desc => 'SQL from files'
+    method_option :exec, :aliases => '-e', :desc => 'SQL from command line'
+    method_option :output, :aliases => '-o', :desc => 'Save SQL output to file'
+    method_option :csv, :type => :boolean, :desc => 'Report SQL output in CSV format', :default => false
+    def psql_exec
+      postgres_options = options.dup
+      postgres_options.merge!(print: true)
+
+      if options[:csv]
+        postgres_options.merge!(ifs: "\t", ofs: ',')
+      end
+
+      postgres(postgres_options)
+    end
+    default_task :psql_exec
+
+    no_tasks do
+      def log_enabled?
+        false
+      end
+    end
+  end
+end
