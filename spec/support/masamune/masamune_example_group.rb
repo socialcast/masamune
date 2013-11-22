7e3b11557bcea76c07fc1a17cd303bb53a2c96d9
Michael Andrews <michael@socialcast.com>
2013-11-22 11:33:40 -0800

    Add context for test harness

--- spec/support/masamune/masamune_example_group.rb
+++ spec/support/masamune/masamune_example_group.rb
@@ -1,4 +1,10 @@
+require 'masamune/has_context'
+
+# Separate context for test harness itself
 module MasamuneExampleGroup
+  include Masamune::HasContext
+  extend self
+
   def self.included(base)
     base.before do
       Thor.send(:include, Masamune::ThorMute)
