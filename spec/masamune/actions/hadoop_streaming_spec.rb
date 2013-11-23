5eb7154ff626c21a202ce477ff95002ce06bfd48
Michael Andrews <michael@socialcast.com>
2013-11-22 19:01:23 -0800

    Add coverage for hadoop streaming

--- spec/masamune/actions/hadoop_streaming_spec.rb
+++ spec/masamune/actions/hadoop_streaming_spec.rb
@@ -18,5 +18,18 @@ describe Masamune::Actions::HadoopStreaming do
     subject { instance.hadoop_streaming }
 
     it { should be_success }
+
+    context 'with jobflow' do
+      before do
+        instance.stub_chain(:configuration, :elastic_mapreduce).and_return({jobflow: 'j-XYZ'})
+        mock_command(/\Ahadoop/, mock_failure)
+        mock_command(/\Aelastic-mapreduce/, mock_success, StringIO.new('ssh fakehost exit'))
+        mock_command(/\Assh fakehost hadoop/, mock_success)
+      end
+
+      subject { instance.hadoop_streaming }
+
+      it { should be_success }
+    end
   end
 end
