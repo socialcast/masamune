9694c7b9fb9d64692117e0cf8ca27a1843cdb05b
Michael Andrews <michael@socialcast.com>
2014-01-16 14:55:03 -0800

    Use tilt for simple aggregate templates

--- lib/masamune/template.rb
+++ lib/masamune/template.rb
@@ -1,24 +1,30 @@
-module Masamune
-  module Template
-    # From: http://stoneship.org/essays/erb-and-the-context-object/
-    class ERBContext
-      def initialize(hash)
-        hash.each_pair do |key, value|
-          instance_variable_set('@' + key.to_s, value)
-        end
-      end
+require 'tilt'
 
-      def get_binding
-        binding
-      end
+module Masamune
+  class Template
+    def initialize(paths = [])
+      @paths = Array.wrap(paths)
+    end
+
+    def render(template, parameters = {})
+      Tilt.new(resolve_file(template)).render(self, parameters)
+    end
+
+    private
+
+    # TODO unify with resolve_path
+    def resolve_file(partial_file)
+      @paths.map do |path|
+        file = File.join(path, File.basename(partial_file))
+        file if File.exists?(file) && File.file?(file)
+      end.compact.first
     end
 
     class << self
-      # TODO include module for magic include
-      def generate(template, parameters = {})
-        output = ERB.new(File.read(template)).result(ERBContext.new(parameters).get_binding)
+      def render_to_file(template, parameters = {})
+        instance = Template.new(File.dirname(template))
         Tempfile.new('masamune').tap do |file|
-          file.write(output)
+          file.write(instance.render(template, parameters))
           file.close
         end.path
       end
--- spec/masamune/template_spec.rb
+++ spec/masamune/template_spec.rb
@@ -1,13 +1,31 @@
 require 'spec_helper'
 
 describe Masamune::Template do
-  describe '.generate' do
-    let(:template) { File.expand_path('../../fixtures/basic.sql.erb', __FILE__) }
+  describe '.render_to_file' do
 
-    subject do
-      File.read(described_class.generate(template, table: 'zombo'))
+    context 'with simple template' do
+      let(:template) { File.expand_path('../../fixtures/simple.sql.erb', __FILE__) }
+
+      subject do
+        File.read(described_class.render_to_file(template, table: 'zombo'))
+      end
+
+      it { should == "SELECT * FROM zombo;\n" }
     end
 
-    it { should == "SELECT * FROM zombo;\n" }
+    context 'with aggregate template' do
+      let(:template) { File.expand_path('../../fixtures/aggregate.sql.erb', __FILE__) }
+
+      subject do
+        File.read(described_class.render_to_file(template))
+      end
+
+      it do should == <<-EOS.gsub(/^\s*/,'')
+        SHOW TABLES;
+        SELECT * FROM foo;
+        SELECT * FROM bar;
+      EOS
+      end
+    end
   end
 end
