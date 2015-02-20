241fdc0be63231664c8baa61fe1d51df4ba11a30
Michael Andrews <michael@socialcast.com>
2015-02-19 18:51:10 -0800

    Misc Map IO fixes

--- lib/masamune/schema/map.rb
+++ lib/masamune/schema/map.rb
@@ -5,16 +5,21 @@ module Masamune::Schema
     class Buffer
       extend Forwardable
 
-      def_delegators :@store, :headers, :format
       def_delegators :@io, :flush, :path
 
-      def initialize(table, io)
+      def initialize(table)
         @table  = table
         @store  = table.store
-        @io     = io.set_encoding('binary', 'UTF-8', undef: :replace)
+        @lines  = 0
+      end
+
+      def bind(io)
+        @io = io.set_encoding('binary', 'UTF-8', undef: :replace)
+        @csv = nil
       end
 
       def each(&block)
+        raise 'must call Buffer#bind first' unless @io
         CSV.parse(@io, options.merge(headers: @store.headers || @table.columns.keys)) do |data|
           row = Masamune::Schema::Row.new(parent: @table, values: data.to_hash, strict: false)
           yield row.to_hash
@@ -22,10 +27,12 @@ module Masamune::Schema
       end
 
       def append(data)
+        raise 'must call Buffer#bind first' unless @io
         row = Masamune::Schema::Row.new(parent: @table, values: data.to_hash)
-        @io ||= Tempfile.new('masamune')
-        @csv ||= CSV.new(@io, options.merge(headers: row.headers, write_headers: @store.headers))
+        write_headers = @store.headers && @lines < 1
+        @csv ||= CSV.new(@io, options.merge(headers: row.headers, write_headers: write_headers))
         @csv << row.serialize
+        @lines += 1
       end
 
       def options
@@ -77,21 +84,58 @@ module Masamune::Schema
     end
 
     def apply(input_files, output_file)
-      Array.wrap(input_files).each do |input_file|
-        File.open(input_file, 'r') do |input_stream|
-          File.open(output_file, 'a+') do |output_stream|
-            apply_stream(input_stream, output_stream)
+      input_buffer  = Buffer.new(source)
+      output_buffer = Buffer.new(intermediate)
+      self.class.convert_files(input_files).each do |input_file|
+        open_stream(input_file, 'r') do |input_stream|
+          input_buffer.bind(input_stream)
+          open_stream(output_file, 'a+') do |output_stream|
+            output_buffer.bind(output_stream)
+            apply_buffer(input_buffer, output_buffer)
           end
         end
       end
       intermediate
     end
 
+    # FIXME spec coverage
+    def open_stream(file, mode, &block)
+      case file
+      when IO
+        yield file
+      when String, Tempfile
+        File.open(file, mode) do |io|
+          yield io
+        end
+      end
+    end
+
+    # FIXME DRY up
+    class << self
+      def convert_file(file)
+        if file.respond_to?(:path)
+          file.flush if file.respond_to?(:flush) && file.respond_to?(:open?) && file.open?
+          file.path
+        else
+          file
+        end
+      end
+
+      def convert_files(files)
+        case files
+        when Set
+          files.map { |file| convert_file(file) }.to_a
+        when Array
+          files.map { |file| convert_file(file) }.to_a
+        else
+          [convert_file(files)]
+        end
+      end
+    end
+
     private
 
-    def apply_stream(input_stream, output_stream)
-      input_buffer = Buffer.new(source, input_stream)
-      output_buffer = Buffer.new(intermediate, output_stream)
+    def apply_buffer(input_buffer, output_buffer)
       input_buffer.each do |input|
         Array.wrap(function.call(input)).each do |output|
           output_buffer.append output
