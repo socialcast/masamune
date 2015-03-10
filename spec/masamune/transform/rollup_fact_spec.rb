e0356d2fcb56fc92b404a9a0c0c48c0fbd2d2439
Michael Andrews <michael@socialcast.com>
2015-03-10 15:14:23 -0700

    Implement transaction to hourly rollup

--- spec/masamune/transform/rollup_fact_spec.rb
+++ spec/masamune/transform/rollup_fact_spec.rb
@@ -51,15 +51,104 @@ describe Masamune::Transform::RollupFact do
         column 'user_id', type: :integer, index: true, natural_key: true
       end
 
+      fact 'visits', partition: 'y%Ym%m', grain: 'transaction' do
+        references :date
+        references :tenant
+        references :user
+        references :user_agent, insert: true
+        references :feature, insert: true
+        references :session, degenerate: true
+        measure 'total', type: :integer, aggregate: :sum
+      end
+
       fact 'visits', partition: 'y%Ym%m', grain: %w(hourly daily monthly) do
         references :date
         references :tenant
         references :user
         references :user_agent, insert: true
         references :feature, insert: true
-
         measure 'total', type: :integer, aggregate: :sum
       end
+    end
+  end
+
+  context 'with postgres transaction fact' do
+    let(:date) { DateTime.civil(2014,8) }
+    let(:source) { catalog.postgres.visits_transaction_fact }
+    let(:target) { catalog.postgres.visits_hourly_fact }
+
+    subject(:result) { transform.rollup_fact(source, target, date).to_s }
+
+    it 'should eq render rollup_fact template' do
+      is_expected.to eq <<-EOS.strip_heredoc
+        BEGIN;
+
+        DROP TABLE IF EXISTS visits_hourly_fact_y2014m08_stage CASCADE;
+        CREATE TABLE IF NOT EXISTS visits_hourly_fact_y2014m08_stage (LIKE visits_hourly_fact INCLUDING ALL);
+
+        ALTER TABLE visits_hourly_fact_y2014m08_stage ADD CONSTRAINT visits_hourly_fact_y2014m08_stage_time_key_check CHECK (time_key >= 1406851200 AND time_key < 1409529600);
+        ALTER TABLE visits_hourly_fact_y2014m08_stage ADD CONSTRAINT visits_hourly_fact_y2014m08_stage_date_dimension_uuid_fkey FOREIGN KEY (date_dimension_uuid) REFERENCES date_dimension(uuid);
+        ALTER TABLE visits_hourly_fact_y2014m08_stage ADD CONSTRAINT visits_hourly_fact_y2014m08_stage_tenant_dimension_uuid_fkey FOREIGN KEY (tenant_dimension_uuid) REFERENCES tenant_dimension(uuid);
+        ALTER TABLE visits_hourly_fact_y2014m08_stage ADD CONSTRAINT visits_hourly_fact_y2014m08_stage_user_dimension_uuid_fkey FOREIGN KEY (user_dimension_uuid) REFERENCES user_dimension(uuid);
+        ALTER TABLE visits_hourly_fact_y2014m08_stage ADD CONSTRAINT visits_hourly_fact_y2014m08_stage_user_agent_type_id_fkey FOREIGN KEY (user_agent_type_id) REFERENCES user_agent_type(id);
+        ALTER TABLE visits_hourly_fact_y2014m08_stage ADD CONSTRAINT visits_hourly_fact_y2014m08_stage_feature_type_id_fkey FOREIGN KEY (feature_type_id) REFERENCES feature_type(id);
+
+        INSERT INTO
+          visits_hourly_fact_y2014m08_stage (date_dimension_uuid, tenant_dimension_uuid, user_dimension_uuid, user_agent_type_id, feature_type_id, total, time_key)
+        SELECT
+          (SELECT uuid FROM date_dimension d WHERE d.date_epoch = date_dimension.date_epoch ORDER BY d.date_id LIMIT 1),
+          visits_transaction_fact_y2014m08.tenant_dimension_uuid,
+          visits_transaction_fact_y2014m08.user_dimension_uuid,
+          visits_transaction_fact_y2014m08.user_agent_type_id,
+          visits_transaction_fact_y2014m08.feature_type_id,
+          SUM(visits_transaction_fact_y2014m08.total),
+          (visits_transaction_fact_y2014m08.time_key - (visits_transaction_fact_y2014m08.time_key % 3600))
+        FROM
+          visits_transaction_fact_y2014m08
+        JOIN
+          date_dimension
+        ON
+          date_dimension.uuid = visits_transaction_fact_y2014m08.date_dimension_uuid
+        GROUP BY
+          date_dimension.date_epoch,
+          visits_transaction_fact_y2014m08.tenant_dimension_uuid,
+          visits_transaction_fact_y2014m08.user_dimension_uuid,
+          visits_transaction_fact_y2014m08.user_agent_type_id,
+          visits_transaction_fact_y2014m08.feature_type_id,
+          (visits_transaction_fact_y2014m08.time_key - (visits_transaction_fact_y2014m08.time_key % 3600))
+        ;
+
+        CREATE INDEX visits_hourly_fact_y2014m08_stage_date_dimension_uuid_index ON visits_hourly_fact_y2014m08_stage (date_dimension_uuid);
+        CREATE INDEX visits_hourly_fact_y2014m08_stage_tenant_dimension_uuid_index ON visits_hourly_fact_y2014m08_stage (tenant_dimension_uuid);
+        CREATE INDEX visits_hourly_fact_y2014m08_stage_user_dimension_uuid_index ON visits_hourly_fact_y2014m08_stage (user_dimension_uuid);
+        CREATE INDEX visits_hourly_fact_y2014m08_stage_user_agent_type_id_index ON visits_hourly_fact_y2014m08_stage (user_agent_type_id);
+        CREATE INDEX visits_hourly_fact_y2014m08_stage_feature_type_id_index ON visits_hourly_fact_y2014m08_stage (feature_type_id);
+        CREATE INDEX visits_hourly_fact_y2014m08_stage_time_key_index ON visits_hourly_fact_y2014m08_stage (time_key);
+
+        COMMIT;
+
+        BEGIN;
+
+        DROP TABLE IF EXISTS visits_hourly_fact_y2014m08;
+        ALTER TABLE visits_hourly_fact_y2014m08_stage RENAME TO visits_hourly_fact_y2014m08;
+
+        ALTER TABLE visits_hourly_fact_y2014m08 INHERIT visits_hourly_fact;
+        ALTER TABLE visits_hourly_fact_y2014m08 ADD CONSTRAINT visits_hourly_fact_y2014m08_time_key_check CHECK (time_key >= 1406851200 AND time_key < 1409529600) NOT VALID;
+        ALTER TABLE visits_hourly_fact_y2014m08 ADD CONSTRAINT visits_hourly_fact_y2014m08_date_dimension_uuid_fkey FOREIGN KEY (date_dimension_uuid) REFERENCES date_dimension(uuid) NOT VALID;
+        ALTER TABLE visits_hourly_fact_y2014m08 ADD CONSTRAINT visits_hourly_fact_y2014m08_tenant_dimension_uuid_fkey FOREIGN KEY (tenant_dimension_uuid) REFERENCES tenant_dimension(uuid) NOT VALID;
+        ALTER TABLE visits_hourly_fact_y2014m08 ADD CONSTRAINT visits_hourly_fact_y2014m08_user_dimension_uuid_fkey FOREIGN KEY (user_dimension_uuid) REFERENCES user_dimension(uuid) NOT VALID;
+        ALTER TABLE visits_hourly_fact_y2014m08 ADD CONSTRAINT visits_hourly_fact_y2014m08_user_agent_type_id_fkey FOREIGN KEY (user_agent_type_id) REFERENCES user_agent_type(id) NOT VALID;
+        ALTER TABLE visits_hourly_fact_y2014m08 ADD CONSTRAINT visits_hourly_fact_y2014m08_feature_type_id_fkey FOREIGN KEY (feature_type_id) REFERENCES feature_type(id) NOT VALID;
+
+        ALTER INDEX visits_hourly_fact_y2014m08_stage_date_dimension_uuid_index RENAME TO visits_hourly_fact_y2014m08_date_dimension_uuid_index;
+        ALTER INDEX visits_hourly_fact_y2014m08_stage_tenant_dimension_uuid_index RENAME TO visits_hourly_fact_y2014m08_tenant_dimension_uuid_index;
+        ALTER INDEX visits_hourly_fact_y2014m08_stage_user_dimension_uuid_index RENAME TO visits_hourly_fact_y2014m08_user_dimension_uuid_index;
+        ALTER INDEX visits_hourly_fact_y2014m08_stage_user_agent_type_id_index RENAME TO visits_hourly_fact_y2014m08_user_agent_type_id_index;
+        ALTER INDEX visits_hourly_fact_y2014m08_stage_feature_type_id_index RENAME TO visits_hourly_fact_y2014m08_feature_type_id_index;
+        ALTER INDEX visits_hourly_fact_y2014m08_stage_time_key_index RENAME TO visits_hourly_fact_y2014m08_time_key_index;
+
+        COMMIT;
+      EOS
     end
   end
 
