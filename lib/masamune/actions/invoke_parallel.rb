0f9764fc7113639d501ca519e7c15bd89a96258b
Michael Andrews <michael@socialcast.com>
2015-04-14 14:30:39 -0700

    Add interactive option to execute

---
+++ lib/masamune/actions/invoke_parallel.rb
@@ -0,0 +1,73 @@
+#  The MIT License (MIT)
+#
+#  Copyright (c) 2014-2015, VMware, Inc. All Rights Reserved.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+require 'active_support/concern'
+require 'parallel'
+
+module Masamune::Actions
+  module InvokeParallel
+    extend ActiveSupport::Concern
+
+    include Masamune::Actions::Execute
+
+    included do |base|
+      base.class_option :max_tasks, aliases: '-p', :type => :numeric, :desc => 'Maximum number of tasks to execute in parallel', :default => 4
+    end
+
+    def invoke_parallel(*task_group)
+      opts = task_group.last.is_a?(Hash) ? task_group.pop.dup : {}
+      max_tasks = [opts.delete(:max_tasks), task_group.count].min
+      console("Setting max_tasks to #{max_tasks}")
+      bail_fast task_group, opts if opts[:version]
+      Parallel.each(task_group, in_processes: max_tasks) do |task_name|
+        begin
+          execute(thor_wrapper, task_name, *task_args(opts), interactive: false, detach: false)
+        rescue SystemExit
+        end
+      end
+    end
+
+    private
+
+    def thor_wrapper
+      'thor'
+    end
+
+    def bail_fast(task_group, opts = {})
+      task_name = task_group.first
+      execute($PROGRAM_NAME, task_name, *task_args(opts))
+      exit
+    end
+
+    def task_args(opts = {})
+      opts.map do |k,v|
+        case v
+        when true
+          "--#{k.to_s.gsub('_', '-')}"
+        when false
+        else
+          ["--#{k.to_s.gsub('_', '-')}", v]
+        end
+      end.flatten.compact
+    end
+  end
+end
--- lib/masamune/commands/shell.rb
+++ lib/masamune/commands/shell.rb
@@ -41,13 +41,13 @@ module Masamune::Commands
       @stderr_line_no = 0
     end
 
-    def replace
+    def replace(opts = {})
       logger.debug('replace: ' + command_args.join(' '))
       before_execute
       around_execute do
         pid = Process.fork
         if pid
-          STDIN.close; STDOUT.close; STDERR.close
+          STDIN.close; STDOUT.close; STDERR.close if opts.fetch(:detach, true)
           Process.waitpid(pid)
           exit
         else
