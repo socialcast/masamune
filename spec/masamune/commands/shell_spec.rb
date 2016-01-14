1e9f1b1b972c397a0d01d3d94f55feca18feb426
Michael Andrews <mjands@gmail.com>
2016-01-14 13:16:47 -0800

    Improve Shell debug output and add coverage

--- spec/masamune/commands/shell_spec.rb
+++ spec/masamune/commands/shell_spec.rb
@@ -35,6 +35,8 @@ describe Masamune::Commands::Shell do
       let(:command) { %Q{bash -c "echo 'stdout 1'; echo 'stderr 1' 1>&2; echo 'stdout 2'; echo 'stderr 2' 1>&2"} }
 
       before do
+        expect(instance.logger).to receive(:debug).with(/\Aexecute: TZ=UTC bash -c .*\z/)
+        expect(instance.logger).to receive(:debug).with(/\Astatus: .* exit 0\z/)
         subject
       end
 
@@ -47,6 +49,8 @@ describe Masamune::Commands::Shell do
       let(:command) { %Q{bash -c 'exit 1'} }
 
       before do
+        expect(instance.logger).to receive(:debug).with(%q(execute: TZ=UTC bash -c 'exit 1'))
+        expect(instance.logger).to receive(:debug).with(/\Astatus: .* exit 1\z/)
         subject
       end
 
@@ -58,11 +62,22 @@ describe Masamune::Commands::Shell do
     context 'with fail_fast and simple command that fails' do
       let(:command) { %Q{bash -c 'exit 1'} }
       let(:options) { {fail_fast: true} }
+
+      before do
+        expect(instance.logger).to receive(:debug).with(%q(execute: TZ=UTC bash -c 'exit 1'))
+        expect(instance.logger).to receive(:debug).with(/\Astatus: .* exit 1\z/)
+      end
+
       it { expect { subject }.to raise_error RuntimeError, "fail_fast: #{command}" }
     end
 
     context 'when command is interrupted' do
       let(:command) { %Q{bash -c "echo 'test'"} }
+
+      before do
+        expect(instance.logger).to receive(:debug).with(%q(execute: TZ=UTC bash -c "echo 'test'"))
+        expect(instance.logger).to receive(:debug).with(/status: .* exit 0/)
+      end
 
       before do
         expect(delegate).to receive(:after_execute) { raise Interrupt }
@@ -77,6 +92,11 @@ describe Masamune::Commands::Shell do
       let(:input) { "ping\npong" }
 
       before do
+        expect(instance.logger).to receive(:debug).with('execute: TZ=UTC cat')
+        expect(instance.logger).to receive(:debug).with(/\Astatus: .* exit 0\z/)
+      end
+
+      before do
         subject
       end
 
@@ -88,6 +108,10 @@ describe Masamune::Commands::Shell do
     context 'with simple command with not-ready input' do
       let(:command) { 'cat' }
       let(:input) { "ping\npong" }
+
+      before do
+        expect(instance.logger).to receive(:debug).with('execute: TZ=UTC cat')
+      end
 
       before do
         expect_any_instance_of(IO).to receive(:wait_writable).and_return(nil)
